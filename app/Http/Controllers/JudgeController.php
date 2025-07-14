6243106c71b3c04b98c3c8fcc67a5874e6d0ec31
JonathanNorth <northj@uwindsor.ca>
2025-07-14 10:35:27 -0400

    Created Judge Files

---
+++ app/Http/Controllers/JudgeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Judge;
+use App\Http\Requests\StoreJudgeRequest;
+use App\Http\Requests\UpdateJudgeRequest;
+
+class JudgeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJudgeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Judge $judge)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Judge $judge)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJudgeRequest $request, Judge $judge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Judge $judge)
+    {
+        //
+    }
+}
