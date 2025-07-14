77dbf018161812db2f10ec4ff6cdd70306c86072
JonathanNorth <northj@uwindsor.ca>
2025-07-14 10:33:35 -0400

    commented out route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\Addbook;
-use App\Http\Controllers\BookController;
 use App\Models\Round;
+use App\Models\Juding;
+use App\Http\Controllers\Addbook;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\RoundController;
 use Pest\Mutate\Mutators\Math\RoundToCeil;
+use App\Http\Controllers\JudgingController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SuggestionController;
 
@@ -25,6 +27,8 @@ Route::get('/get-judges',[RoundController::class, 'getJudges'])->name('get.judge
 Route::post('/add-book', [BookController::class, 'storeBook'])->middleware(['auth','verified'])->name('add.book');
 
 Route::get('/my-books', [BookController::class, 'myBooks'])->middleware(['auth','verified'])->name('my.books');
+
+//Route::get('/display-juding', [JudgingController::class, 'displayJudging'])->middleware(['auth','verified'])->name('display.judging');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
